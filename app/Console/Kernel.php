2fd40f0ce8aa7382fb135829ccf46fb8a54f9e02
amanbaevsayat <amanbaevsayat@gmail.com>
2020-11-25 12:19:21 +0600

    refactor schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,44 +36,13 @@ class Kernel extends ConsoleKernel
 
         $schedule->command("load --rates")->dailyAt("03:40")->appendOutputTo($filePath);
 
+        $this->solveDate($yesterday, $filePath, $schedule);
+
         if (date("D", strtotime($today)) == "Mon") {
-
-            /* SEED */
-            $schedule->command("seed --contacts --startDate={$thisWeekStart} --endDate={$thisWeekEnd}")->dailyAt("02:00")->appendOutputTo($filePath);
-            $schedule->command("seed --team-outcomes --startDate={$thisWeekStart} --endDate={$thisWeekEnd}")->dailyAt("02:10")->appendOutputTo($filePath);
-            $schedule->command("seed --custom-outcomes --startDate={$thisWeekStart} --endDate={$thisWeekEnd}")->dailyAt("02:20")->appendOutputTo($filePath);
-
-            /* LOAD */
-            $schedule->command("load --masters")->dailyAt("03:10")->appendOutputTo($filePath);
-            $schedule->command("load --cosmetologists")->dailyAt("03:20")->appendOutputTo($filePath);
-            $schedule->command("load --services")->dailyAt("03:30")->appendOutputTo($filePath);
-            $schedule->command("load --records --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("03:50")->appendOutputTo($filePath);
-
-            /* SOLVE */
-            $schedule->command("solve --total-comission --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:00")->appendOutputTo($filePath);
-            $schedule->command("solve --masters-comission --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:10")->appendOutputTo($filePath);
-            $schedule->command("solve --masters-profit --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:20")->appendOutputTo($filePath);
-            $schedule->command("solve --custom-outcomes --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:30")->appendOutputTo($filePath);
-            $schedule->command("solve --managers-profit --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:40")->appendOutputTo($filePath);
-            $schedule->command("solve --operators-profit --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:50")->appendOutputTo($filePath);
-            $schedule->command("solve --masters-penalty --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:55")->appendOutputTo($filePath);
-        } else {
-
-            /* LOAD */
-            $schedule->command("load --masters")->dailyAt("03:10")->appendOutputTo($filePath);
-            $schedule->command("load --cosmetologists")->dailyAt("03:20")->appendOutputTo($filePath);
-            $schedule->command("load --services")->dailyAt("03:30")->appendOutputTo($filePath);
-            $schedule->command("load --records --startDate={$yesterday} --endDate={$yesterday}")->dailyAt("03:50")->appendOutputTo($filePath);
-
-            /* SOLVE */
-            $schedule->command("solve --total-comission --date={$yesterday}")->dailyAt("04:00")->appendOutputTo($filePath);
-            $schedule->command("solve --masters-comission --date={$yesterday}")->dailyAt("04:10")->appendOutputTo($filePath);
-            $schedule->command("solve --masters-profit --date={$yesterday}")->dailyAt("04:20")->appendOutputTo($filePath);
-            $schedule->command("solve --custom-outcomes --date={$yesterday}")->dailyAt("04:30")->appendOutputTo($filePath);
-            $schedule->command("solve --managers-profit --date={$yesterday}")->dailyAt("04:40")->appendOutputTo($filePath);
-            $schedule->command("solve --operators-profit --date={$yesterday}")->dailyAt("04:50")->appendOutputTo($filePath);
-            $schedule->command("solve --masters-penalty --date={$yesterday}")->dailyAt("04:55")->appendOutputTo($filePath);
-
+            $this->seedNewWeek($thisWeekStart, $thisWeekEnd, $filePath, $schedule);
+        }
+        if (date("D", strtotime($today)) == "Tue") {
+            $this->solveLastWeek($lastWeekStart, $lastWeekEnd, $filePath, $schedule);
         }
     }
 
@@ -87,5 +56,49 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__ . "/Commands");
 
         require base_path("routes/console.php");
+    }
+
+    private function solveDate(string $date, string $filePath, Schedule $schedule)
+    {
+        /* LOAD */
+        $schedule->command("load --masters")->dailyAt("03:10")->appendOutputTo($filePath);
+        $schedule->command("load --cosmetologists")->dailyAt("03:20")->appendOutputTo($filePath);
+        $schedule->command("load --services")->dailyAt("03:30")->appendOutputTo($filePath);
+        $schedule->command("load --records --startDate={$date} --endDate={$date}")->dailyAt("03:50")->appendOutputTo($filePath);
+
+        /* SOLVE */
+        $schedule->command("solve --total-comission --date={$date}")->dailyAt("04:00")->appendOutputTo($filePath);
+        $schedule->command("solve --masters-comission --date={$date}")->dailyAt("04:10")->appendOutputTo($filePath);
+        $schedule->command("solve --masters-profit --date={$date}")->dailyAt("04:20")->appendOutputTo($filePath);
+        $schedule->command("solve --custom-outcomes --date={$date}")->dailyAt("04:30")->appendOutputTo($filePath);
+        $schedule->command("solve --managers-profit --date={$date}")->dailyAt("04:40")->appendOutputTo($filePath);
+        $schedule->command("solve --operators-profit --date={$date}")->dailyAt("04:50")->appendOutputTo($filePath);
+        $schedule->command("solve --masters-penalty --date={$date}")->dailyAt("04:55")->appendOutputTo($filePath);
+    }
+
+    private function solveLastWeek(string $lastWeekStart, string $lastWeekEnd, string $filePath, Schedule $schedule)
+    {
+        /* LOAD */
+        $schedule->command("load --masters")->dailyAt("03:10")->appendOutputTo($filePath);
+        $schedule->command("load --cosmetologists")->dailyAt("03:20")->appendOutputTo($filePath);
+        $schedule->command("load --services")->dailyAt("03:30")->appendOutputTo($filePath);
+        $schedule->command("load --records --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("03:50")->appendOutputTo($filePath);
+
+        /* SOLVE */
+        $schedule->command("solve --total-comission --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:00")->appendOutputTo($filePath);
+        $schedule->command("solve --masters-comission --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:10")->appendOutputTo($filePath);
+        $schedule->command("solve --masters-profit --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:20")->appendOutputTo($filePath);
+        $schedule->command("solve --custom-outcomes --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:30")->appendOutputTo($filePath);
+        $schedule->command("solve --managers-profit --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:40")->appendOutputTo($filePath);
+        $schedule->command("solve --operators-profit --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:50")->appendOutputTo($filePath);
+        $schedule->command("solve --masters-penalty --startDate={$lastWeekStart} --endDate={$lastWeekEnd}")->dailyAt("04:55")->appendOutputTo($filePath);
+    }
+
+    private function seedNewWeek(string $thisWeekStart, string $thisWeekEnd, string $filePath, Schedule $schedule)
+    {
+        /* SEED */
+        $schedule->command("seed --contacts --startDate={$thisWeekStart} --endDate={$thisWeekEnd}")->dailyAt("02:00")->appendOutputTo($filePath);
+        $schedule->command("seed --team-outcomes --startDate={$thisWeekStart} --endDate={$thisWeekEnd}")->dailyAt("02:10")->appendOutputTo($filePath);
+        $schedule->command("seed --custom-outcomes --startDate={$thisWeekStart} --endDate={$thisWeekEnd}")->dailyAt("02:20")->appendOutputTo($filePath);
     }
 }
